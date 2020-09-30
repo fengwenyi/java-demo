6132e7e924961101f1dac14e3f81c658e656e38c
冯文议 <xfsy_2015@163.com>
2020-09-30 14:42:58 +0800

    feat(ArrayList): ArrayList源码阅读

--- src/main/java/com/fengwenyi/javademo/ArrayListDemo.java
+++ src/main/java/com/fengwenyi/javademo/ArrayListDemo.java
@@ -11,6 +11,15 @@ public class ArrayListDemo {
     public static void main(String[] args) {
         ArrayList arrayList = new ArrayList();
         // arrayList.subList();
+        Object clone = arrayList.clone();
+
+        int size = 10;
+        System.out.println(Integer.toBinaryString(size));
+        size = size >> 1;
+        System.out.println(Integer.toBinaryString(size));
+        size = size << 2;
+        System.out.println(size);
+        System.out.println(Integer.toBinaryString(size));
     }
 
 }
