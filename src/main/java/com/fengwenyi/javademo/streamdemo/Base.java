adeb41bb88f985805882d97bf227d4b6314d5309
冯文议 <xfsy_2015@163.com>
2021-04-16 17:48:08 +0800

    java 8 stream demo

---
+++ src/main/java/com/fengwenyi/javademo/streamdemo/Base.java
@@ -0,0 +1,37 @@
+package com.fengwenyi.javademo.streamdemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Erwin Feng
+ * @since 2020/4/27 2:09
+ */
+public class Base {
+
+    protected static List<Employee> list = Arrays.asList(
+            new Employee(1, "Jacob", 1000.00),
+            new Employee(2, "Sophia", 2000.00),
+            new Employee(3, "Rose", 3000.00),
+            new Employee(4, "Lily", 4000.00),
+            new Employee(5, "Daisy", 5000.00),
+            new Employee(6, "Jane", 5000.00),
+            new Employee(7, "Jasmine", 6000.00),
+            new Employee(8, "Jack", 6000.00),
+            new Employee(9, "Poppy", 7000.00)
+    );
+
+    protected static List<List<Employee>> multidimensionalList = getMultidimensionalList();
+
+    // 我们将薪资1000-3000,4000-500,6000-7000分别放在一起，然后在组合放在一起
+    private static List<List<Employee>> getMultidimensionalList() {
+        multidimensionalList = new ArrayList<>();
+        multidimensionalList.add(list.stream().filter(employee -> employee.getSalary() >= 1000 && employee.getSalary() <= 3000).collect(Collectors.toList()));
+        multidimensionalList.add(list.stream().filter(employee -> employee.getSalary() >= 4000 && employee.getSalary() <= 5000).collect(Collectors.toList()));
+        multidimensionalList.add(list.stream().filter(employee -> employee.getSalary() >= 6000 && employee.getSalary() <= 7000).collect(Collectors.toList()));
+        return multidimensionalList;
+    }
+
+}
---
+++ src/main/java/com/fengwenyi/javademo/streamdemo/Leader.java
@@ -0,0 +1,42 @@
+package com.fengwenyi.javademo.streamdemo;
+
+/**
+ * @author Erwin Feng
+ * @since 2020/5/13
+ */
+public class Leader {
+
+    /** 员工姓名 */
+    private String name;
+
+    /** 员工薪酬 */
+    private Double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Leader() {
+    }
+
+    @Override
+    public String toString() {
+        return "Leader{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+}
