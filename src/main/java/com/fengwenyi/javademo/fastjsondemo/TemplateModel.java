2f5728a96a10237c98c428f3804a08a522ab1294
ErwinFeng <xfsy_2015@163.com>
2022-06-15 14:18:41 +0800

    测试使用fastjson进行模版占位

---
+++ src/main/java/com/fengwenyi/javademo/fastjsondemo/TemplateModel.java
@@ -0,0 +1,46 @@
+package com.fengwenyi.javademo.fastjsondemo;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fengwenyi.javalib.util.StringUtils;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2022-06-15
+ */
+@Data
+@Slf4j
+public class TemplateModel {
+
+    @JSONField(name = "NAME")
+    private String name;
+
+
+    /**
+     * 对模板参数进行转换
+     * @param content 待转换的内容
+     * @return 转换后的内容
+     */
+    public String convert(String content) {
+        String paramsStr = JSON.toJSONString(this);
+        Map<String, Object> paramsMap = JSON.parseObject(paramsStr, new TypeReference<>() {});
+        for (String key : paramsMap.keySet()) {
+            if (StringUtils.isNotEmpty(key) && content.contains(key)) {
+                Object value = paramsMap.get(key);
+                if (Objects.isNull(value)) {
+                    log.error("将模板占位替换成值-值为空-该占位值为{}", key);
+                } else {
+                    content = content.replaceAll(key, value.toString());
+                }
+            }
+        }
+        return content;
+    }
+
+}
