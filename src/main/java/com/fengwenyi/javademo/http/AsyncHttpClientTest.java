f9f5670338c700b13f7a9f417ce6f5062b670e97
ErwinFeng <xfsy_2015@163.com>
2022-07-05 17:56:50 +0800

    async http client example test

---
+++ src/main/java/com/fengwenyi/javademo/http/AsyncHttpClientTest.java
@@ -0,0 +1,29 @@
+package com.fengwenyi.javademo.http;
+
+import com.fengwenyi.javalib.convert.JsonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Response;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2022-07-05
+ */
+@Slf4j
+public class AsyncHttpClientTest {
+
+    @Test
+    public void testHttpBasicExample() throws ExecutionException, InterruptedException {
+        AsyncHttpClient asyncHttpClient = asyncHttpClient();
+        Future<Response> whenResponse = asyncHttpClient.prepareGet("https://baidu.com").execute();
+        Response response = whenResponse.get();
+        log.info(JsonUtils.convertString(response.getResponseBody()));
+    }
+
+}
