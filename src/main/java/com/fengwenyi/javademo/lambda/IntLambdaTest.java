4071b629b2c399ec694d63f979a0dbfba089d31b
ErwinFeng <xfsy_2015@163.com>
2022-08-29 15:21:32 +0800

    通过字符串转int 理解lambda表达式的写法，含方法引用

---
+++ src/main/java/com/fengwenyi/javademo/lambda/IntLambdaTest.java
@@ -0,0 +1,39 @@
+package com.fengwenyi.javademo.lambda;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2022-08-29
+ */
+public class IntLambdaTest {
+
+    public static void main(String[] args) {
+
+        Demo demo1 = new Demo() {
+            @Override
+            public int convert(String s) {
+                return Integer.parseInt(s);
+            }
+        };
+        System.out.println(demo1.convert("111"));
+
+        Demo demo2 = s -> {
+                return Integer.parseInt(s);
+            };
+        System.out.println(demo2.convert("222"));
+
+        Demo demo3 = s -> Integer.parseInt(s);
+        System.out.println(demo3.convert("333"));
+
+        Demo demo4 = Integer::parseInt;
+        System.out.println(demo4.convert("444"));
+
+        Demo demo5 = Integer::new;
+        System.out.println(demo4.convert("555"));
+
+    }
+
+}
+
+interface Demo {
+    int convert(String s);
+}
