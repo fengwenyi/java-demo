f5bbcb4a704bf8fd0778f8c30321a40d23b96a33
ErwinFeng <xfsy_2015@163.com>
2022-11-10 10:39:58 +0800

    optional if present

---
+++ src/main/java/com/fengwenyi/javademo/java8/OptionalTests.java
@@ -0,0 +1,33 @@
+package com.fengwenyi.javademo.java8;
+
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.Optional;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2022-11-10
+ */
+public class OptionalTests {
+
+    @Test
+    public void testIfPresent() {
+        User user = new User();
+
+        String name = "张三";
+        Optional.ofNullable(name).ifPresent(user::setName);
+
+        System.out.println(user);
+    }
+
+    @Data
+    static class User {
+
+        private int id;
+
+        private String name;
+
+    }
+
+}
