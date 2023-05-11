367f232e9eb5d2990dd8a79623a473a55e2fc154
ErwinFeng <xfsy_2015@163.com>
2023-05-11 15:43:15 +0800

    优化 function map 转换 demo

--- doc/Function.md
+++ doc/Function.md
@@ -1,2 +1,39 @@
 # Function
 
+```java
+@FunctionalInterface
+public interface Function<T, R> {
+    
+    R apply(T t);
+
+}
+```
+
+T: 入参 <br>
+R: 返参
+
+简单示例：
+
+平方函数
+
+```java
+public static Function<Integer, Integer> square() {
+    return num -> num * num;
+}
+```
+
+调用：
+
+```java
+int result = square().apply(5);
+System.out.println(result);
+```
+
+为什么我们需要花费时间精力上手函数式编程而不是直接用面对对象解决问题？
+
+- 函数式编程的引入，帮助我们减少业务逻辑和代码的分歧。他允许我们在更高层次更自然的描述业务逻辑。让代码直接描述“你想做什么”，而不是“你想怎样去做”。
+- 许多样板（boilerplates）可以被移除，这会让代码更清晰更简洁。
+- 高阶函数（Higher-order functions）允许我们：　　 
+  - 发送方法到其他方法中。
+  - 在其他方法中创建方法。
+  - 从其他方法中返回方法。
--- src/main/java/com/fengwenyi/javademo/functiondemo/FunctionDemo.java
+++ src/main/java/com/fengwenyi/javademo/functiondemo/FunctionDemo.java
@@ -1,6 +1,10 @@
 package com.fengwenyi.javademo.functiondemo;
 
+import com.fengwenyi.javalib.convert.JsonUtils;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 /**
@@ -10,7 +14,24 @@ import java.util.function.Function;
 public class FunctionDemo {
 
     public static void main(String[] args) {
-        Function<Map<String, String>, User> mapUserFunction = getMap();
+        Map<String, String> map = new HashMap<>();
+        map.put("user", "张三");
+        map.put("age", "18");
+        map.put("gender", "男");
+        Function<Map<String, String>, User> function = mapToUser();
+        User user = function.apply(map);
+        System.out.println(JsonUtils.prettyPrint(user));
+    }
+
+
+    private static Function<Map<String, String>, User> mapToUser() {
+        return map -> {
+            User user = new User();
+            user.name = map.get("user");
+            user.age = Integer.valueOf(map.get("age"));
+            user.gender = map.get("gender");
+            return user;
+        };
     }
 
     private static Function<Map<String, String>, User> getMap() {
@@ -23,10 +44,74 @@ public class FunctionDemo {
         };
     }
 
+    private static void test1() {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("user", "张三");
+        map.put("age", "18");
+        map.put("gender", "男");
+
+        Function<Map<String, String>, User> func = getMap2();
+        User user = func.apply(map);
+        System.out.println(user);
+    }
+
+    private static Function<Map<String, String>, User> getMap2() {
+        return map -> {
+            User user = new User();
+            user.name = map.get("user");
+            user.age = Integer.valueOf(map.get("age"));
+            user.gender = map.get("gender");
+            return user;
+        };
+    }
+
     static class User {
         String name;
         Integer age;
         String gender;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
     }
+
+    public static Function<Integer, Integer> square() {
+        return num -> num * num;
+    }
+
+    public static class LambdaQueryWrapper<T> {
+
+        public LambdaQueryWrapper eq(Function<T, Object> func, Object value) {
+            return this;
+        }
+
+    }
+
+    public static void testBuildLambdaQueryWrapper() {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getName, "张三");
+    }
+
 
 }
